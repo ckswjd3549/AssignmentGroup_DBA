6c22aa494d5c2e7d9b3f16ed673510296115d0a1
ckswjd3549 <s3712688@rmit.edu.vn>
2021-09-10 16:08:01 +0900

    hello

--- AuctionMarket/database/biddingdb.sql
+++ AuctionMarket/database/biddingdb.sql
@@ -2,63 +2,82 @@
 # CREATE SCHEMA bidding_system;
 # USE bidding_system;
 CREATE TABLE IF NOT EXISTS branch(
-    code INT(30) NOT NULL,
+    code INT NOT NULL,
     name varchar(30) NOT NULL,
-    address varchar(30) NOT NULL,
+    address varchar(200) NOT NULL,
     hotline_number varchar(15) NOT NULL,
     UNIQUE(code, name)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table branch
+INSERT INTO branch values (1, 'Online bidding system', '702 Nguyen Van Linh, Tan Hung, Quan 7, Thanh pho Ho Chi Minh 700000', '0778139985');
 
 -- Table structure for table account
-
 CREATE TABLE IF NOT EXISTS account(
-    Email varchar(200) NOT NULL,
-    Phone varchar(30) NOT NULL,
-    Password varchar(30) NOT NULL,
-    First_name varchar(30) NOT NULL,
-    Last_name varchar(30) NOT NULL,
-    Id_num varchar(30) NOT NULL,
-    UID INT NOT NULL,
-    Address varchar(200) NOT NULL,
-    Aity varchar(30) NOT NULL,
-    Country varchar(30) NOT NULL,
-    Balance float NOT NULL,
-    Admin boolean default false,
-    UNIQUE(UID, Email, Phone)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    email varchar(200) NOT NULL,
+    phone varchar(30) NOT NULL,
+    password varchar(30) NOT NULL,
+    first_name varchar(30) NOT NULL,
+    last_name varchar(30) NOT NULL,
+    id INT(30) NOT NULL,
+    address varchar(200) NOT NULL,
+    city varchar(30) NOT NULL,
+    country varchar(30) NOT NULL,
+    profile_image text NOT NULL,
+    balance float NOT NULL,
+    UNIQUE(id, email, phone)
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data is automatically executed in register.php page.
 
 -- Table structure for table bidding
 CREATE TABLE IF NOT EXISTS bidding(
-    id INT(30) NOT NULL,
-    user_id VARCHAR(30) NOT NULL,
-    product_id VARCHAR(30) NOT NULL,
-    amount float NOT NULL,
+                                      id INT NOT NULL,
+                                      user_id INT(30) NOT NULL,
+    product_id INT(30) NOT NULL,
+    amount INT NOT NULL,
     status tinyint(1) NOT NULL DEFAULT 1 COMMENT '1=bid,2=confirmed,3=cancelled',
     date_created datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-    UNIQUE(id, user_id, product_id)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    UNIQUE(id, user_id, product_id, amount)
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table bidding
+INSERT INTO bidding VALUES(1, 5, 1, 300, 1, '2020-10-27 14:18:50');
+INSERT INTO bidding VALUES(4, 5, 3, 155000, 1, '2020-10-27 16:37:29');
 
 -- Table structure for table category
 CREATE TABLE IF NOT EXISTS category(
-    id INT(30) NOT NULL,
-    name VARCHAR(30) NOT NULL,
+                                       id INT NOT NULL,
+                                       name VARCHAR(30) NOT NULL,
     UNIQUE(id, name)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table category
+INSERT INTO category VALUES(1, 'Sample Category'),
+                           (2, 'Appliances'),
+                           (3, 'Desktop Computers'),
+                           (4, 'Laptop'),
+                           (5, 'Mobile Phone'),
+                           (6, 'hobbies'),
+                           (7, 'history');
 
 -- Table structure for table product
 CREATE TABLE IF NOT EXISTS product(
-    id INT(30) NOT NULL,
-    category_id VARCHAR(30) NOT NULL,
+                                      id INT NOT NULL,
+                                      category_id INT(30) NOT NULL,
     name VARCHAR(30) NOT NULL,
     description TEXT NOT NULL,
-    start_bid float NOT NULL,
+    bid_start_time float NOT NULL,
     price float NOT NULL,
-    bid_start_time datetime NOT NULL,
     bid_end_time datetime NOT NULL,
     image text NOT NULL,
     date_created datetime NOT NULL DEFAULT current_timestamp(),
-    UNIQUE(id, category_id, name, bid_end_time)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+    UNIQUE(id, category_id, name)
+    )ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- Dumping data for table product
+INSERT INTO product VALUES(1, 5, 'Sample Smart Phone', 'Sample only', 7000, 7000, '2020-10-27 19:00:00', '1.jpg', '2020-10-27 09:50:54'),
+                          (3, 1, 'Gadget Package', 'Sample ', 150000, 15000, '2020-10-27 17:00:00', '3.jpg', '2020-10-27 09:59:39');
 
 # DROP TABLE IF EXISTS account;
 # DROP TABLE IF EXISTS bidding;
@@ -68,7 +87,7 @@ CREATE TABLE IF NOT EXISTS product(
 
 -- Indexes for table bidding
 ALTER TABLE bidding
-    ADD PRIMARY KEY (id);
+    ADD PRIMARY KEY (id, amount);
 
 -- Indexes for table category
 ALTER TABLE category
@@ -76,7 +95,7 @@ ALTER TABLE category
 
 -- Indexes for table product
 ALTER TABLE product
-    ADD PRIMARY KEY (id, bid_end_time);
+    ADD PRIMARY KEY (id);
 
 -- Indexes for table product
 ALTER TABLE branch
@@ -88,50 +107,66 @@ ALTER TABLE account
 
 -- AUTO_INCREMENT for table bidding
 ALTER TABLE bidding
-    MODIFY id INT(30) NOT NULL AUTO_INCREMENT;
+    MODIFY id INT NOT NULL AUTO_INCREMENT;
 
 -- AUTO_INCREMENT for table category
 ALTER TABLE category
-    MODIFY id INT(30) NOT NULL AUTO_INCREMENT;
+    MODIFY id INT NOT NULL AUTO_INCREMENT;
 
 -- AUTO_INCREMENT for table product
 ALTER TABLE product
-    MODIFY id INT(30) NOT NULL AUTO_INCREMENT;
+    MODIFY id INT NOT NULL AUTO_INCREMENT;
 
 -- AUTO_INCREMENT for table branch
 ALTER TABLE branch
-    MODIFY code INT(30) NOT NULL AUTO_INCREMENT;
+    MODIFY code INT NOT NULL AUTO_INCREMENT;
 
 -- AUTO_INCREMENT for table account
 ALTER TABLE account
-    MODIFY id INT(30) NOT NULL AUTO_INCREMENT;
+    MODIFY id INT NOT NULL AUTO_INCREMENT;
 
-ALTER TABLE account
-    ADD INDEX idx_country (country);
-SELECT * from account where country = 'Vietnam';
+ALTER TABLE category
+    ADD INDEX idx_country (name);
 
-ALTER TABLE product
-    ADD INDEX idx_name (name);
-SELECT * from product where name like 'New%';
+SELECT * from category where name = 'Appliances';
 
-ALTER TABLE product
-    PARTITION BY RANGE COLUMNS(bid_end_time)(
-    PARTITION p0 VALUES LESS THAN ('2020-01-01'),
-    PARTITION p1 VALUES LESS THAN ('2020-03-01'),
-    PARTITION p2 VALUES LESS THAN ('2020-04-01'),
-    PARTITION p3 VALUES LESS THAN ('2020-06-01'),
-    PARTITION p4 VALUES LESS THAN ('2020-07-01'),
-    PARTITION p5 VALUES LESS THAN ('2020-10-01'),
-    PARTITION p6 VALUES LESS THAN ('2020-11-01'),
-    PARTITION p7 VALUES LESS THAN ('2020-12-01')
+ALTER TABLE bidding
+    PARTITION BY RANGE (amount)(
+    PARTITION p0 VALUES LESS THAN (100),
+    PARTITION p1 VALUES LESS THAN (200),
+    PARTITION p2 VALUES LESS THAN (300),
+    PARTITION p3 VALUES LESS THAN (500),
+    PARTITION p4 VALUES LESS THAN (1000),
+    PARTITION p5 VALUES LESS THAN (2000),
+    PARTITION p6 VALUES LESS THAN (5000),
+    PARTITION p7 VALUES LESS THAN (maxvalue)
     );
 
-ALTER TABLE account
-    PARTITION BY RANGE (id)(
-    PARTITION p0 VALUES LESS THAN (10),
-    PARTITION p1 VALUES LESS THAN (20),
-    PARTITION p2 VALUES LESS THAN (30),
-    PARTITION p3 VALUES LESS THAN (40),
-    PARTITION p4 VALUES LESS THAN (maxvalue)
-    );
+SELECT * FROM bidding where amount = 300;
+
+DELIMITER $$
+
+create procedure sp_bidding_price(in b_id INT, in new_amount INT)
+begin
+update bidding set amount = new_amount
+where id = b_id;
+end $$
+
+DELIMITER ;
+
+call sp_bidding_price('1', 100);
+
+delimiter $$
+
+create trigger tgr_prevent_bidding_amount
+    before update on bidding
+    for each row
+begin
+    if old.amount >= new.amount then
+        signal sqlstate '45000' set message_text = 'Bidding amount must be higher than current price';
+end if;
+end $$
+
+delimiter ;
+
 
